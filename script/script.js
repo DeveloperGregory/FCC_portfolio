981312f9d57d0ed63fb59f51ffa853709a8db16f
DeveloperGregory <ashimar229@gmail.com>
2021-01-18 16:42:43 -0500

    Worked on the style

--- script/script.js
+++ script/script.js
@@ -3,3 +3,10 @@ $(document).ready(function (){
     $('#my-title').addClass('animate__animated animate__lightSpeedInRight');
     
 });
+
+const yearsBRI = document.getElementById('years-BRI');
+const BRISTARTDATE = 2003;
+let date = new Date();
+let currYear = date.getFullYear();
+
+yearsBRI.innerHTML = currYear - BRISTARTDATE;
